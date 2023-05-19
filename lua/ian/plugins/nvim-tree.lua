523a47c72aafdb1b56f44cebaf3fa10bdc144995
Ian-SeoJaeMin <animomin@daum.net>
2023-05-19 16:47:15 +0900

    chore: set nvim config

---
+++ lua/ian/plugins/nvim-tree.lua
@@ -0,0 +1,91 @@
+-- import nvim-tree plugin safely
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+	return
+end
+
+local config_setup, nvimtreeconfig = pcall(require, "nvim-tree.config")
+if not config_setup then
+	return
+end
+
+local tree_cb = nvimtreeconfig.nvim_tree_callback
+
+-- recommended settings from nvim-tree documentation
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- change color for arrows in tree to light blue
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- Mappings migrated from view.mappings.list
+	--
+	-- You will need to insert "your code goes here" for any mappings with a custom action_cb
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+end
+
+-- configure nvim-tree
+nvimtree.setup({
+	-- change folder arrow icons
+	-- renderer = {
+	-- 	icons = {
+	-- 		glyphs = {
+	-- 			folder = {
+	-- 				arrow_closed = "", -- arrow when folder is closed
+	-- 				arrow_open = "", -- arrow when folder is open
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
+	-- disable window_picker for
+	-- explorer to work well with
+	-- window splits
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+	-- 	git = {
+	-- 		ignore = false,
+	-- 	},
+	on_attach = on_attach,
+})
+
+-- open nvim-tree on setup
+
+local function open_nvim_tree(data)
+	-- buffer is a [No Name]
+	local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
+
+	if not no_name and not directory then
+		return
+	end
+
+	-- change to the directory
+	if directory then
+		vim.cmd.cd(data.file)
+	end
+
+	-- open the tree
+	require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
